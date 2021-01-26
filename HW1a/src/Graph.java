5729e67396a5a0089760cb838d792510af4aa4af
jplulu <lujason899@gmail.com>
2021-01-26 17:53:56 -0500

    Added read graph function

--- HW1a/src/Graph.java
+++ HW1a/src/Graph.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Graph {
     private int sourceNode;
     private List<List<Integer>> adjMatrix;
--- HW1a/src/Main.java
+++ HW1a/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Node node = new Node(1,1);
-        System.out.println(node.getDistance());
+
     }
 }
