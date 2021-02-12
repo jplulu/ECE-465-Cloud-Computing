45ee6ede4b32bb4ccfc0ae0e127a3b776910c5ca
jplulu <lujason899@gmail.com>
2021-02-12 13:54:08 -0500

    Multi Node Init

--- multi-node-threaded/src/main/java/edu/cooper/ece465/Worker.java
+++ multi-node-threaded/src/main/java/edu/cooper/ece465/Worker.java
@@ -1,5 +1,32 @@
 package edu.cooper.ece465;
 
-public class Worker {
+import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Worker {
+    public static void main(String[] args) {
+        try(Socket s = new Socket("localhost", 420)){
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            Gson g = new Gson();
+
+            boolean initDone = false;
+            String initData;
+
+            while(!initDone) {
+                if ((initData = br.readLine()) != null) {
+
+                    initDone = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
